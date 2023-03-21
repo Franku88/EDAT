7fdac6534d53c762d56a251b5184f74f99b30970
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2023-03-21 00:29:19 -0300

    21/03/2023

--- lineales.estaticas/src/Pila.java
+++ lineales.estaticas/src/Pila.java
@@ -53,18 +53,45 @@ public class Pila {
  
     public boolean esVacia() {
         //Si el tope es -1, entonces la pila esta vacia
-        boolean vacio = tope <= -1;
+        boolean vacio = (tope <= -1);
         return vacio;
     }
    
     public void vaciar() {
-       
-        for (int i = 0; i < this.tope; i++) {
- 
- 
+        /*Realiza un recorrido desde el tope hasta la posicion 0,
+        asigna null en todas las posiciones, si el tope es -1, entonces
+        el arreglo ya esta vacio*/
+        if (!esVacia()) {
+            for (int i = this.tope; i > -1; i--) {
+                this.arreglo[i] = null;
+            }
+            this.tope = -1;
         }
- 
- 
+    }
+
+    public Pila clon() {
+        //Metodo que crea una instancia con los mismos elementos y tope de la pila en el llamado
+        //Crea clon vacio
+        Pila pila2 = new Pila();
+        //Verifica que la pila no este vacia
+        if (!esVacia()) {
+            //Asigna el mismo tope a pila2
+            pila2.tope = this.tope;
+            //Invoca metodo recursivo con ambos arreglos y el tope de ambas pilas
+            pila2.arreglo = clonRecursivo(this.arreglo, pila2.arreglo, this.tope);
+        }
+        return pila2;
+    }
+
+    private Object[] clonRecursivo(Object[] arreglo1, Object[] arreglo2, int tope) {
+        /*arreglo1: array de pila a clonar, arreglo2: array de clon, tope actual a copiar*/
+        //Corta recursion si sobrepasa posicion 0 de arreglo (copia desde tope hasta 0)
+        if (tope != -1) {
+            //Asigna valor de la posicion tope
+            arreglo2[tope]= arreglo1[tope];
+            //Invocacion recursiva pero con objecto debajo del tope actual
+            clonRecursivo(arreglo1, arreglo2, tope-1);
+        }
+        return arreglo2;
     }
  }
- 
