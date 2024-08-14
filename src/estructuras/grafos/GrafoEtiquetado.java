fcbff3314cfecdd85525fb44af027845a2c4609c
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-08-14 20:08:42 -0300

    14/08/2024 Falta clone de grafo

--- src/estructuras/grafos/GrafoEtiquetado.java
+++ src/estructuras/grafos/GrafoEtiquetado.java
@@ -190,18 +190,16 @@ public class GrafoEtiquetado {
 
     public boolean existeCamino(Object origen, Object destino) {
         boolean exito = false;
-        NodoVert aux = this.inicio; 
         NodoVert auxOrig = null;
         NodoVert auxDest = null;
-        //Busco ambos nodos
-        while (aux != null && (auxOrig == null || auxDest == null)) {
+        NodoVert aux = this.inicio;
+        while (aux != null && (auxOrig == null || auxDest == null)) { //Busco ambos nodos orig y dest
             if (origen.equals(aux.getElemento())) {
                 auxOrig = aux;
-            } else {
-                if (destino.equals(aux.getElemento())) {
-                    auxDest = aux;
-                }
-            } 
+            }
+            if (destino.equals(aux.getElemento())) {
+                auxDest = aux;
+            }
             aux = aux.getSigVertice();
         }
         if (auxOrig != null && auxDest != null) { //Si fueron encontrados, busco un camino
@@ -211,145 +209,143 @@ public class GrafoEtiquetado {
         return exito;
     }
 
-    public boolean existeCaminoAux(NodoVert nodo, Object dest, Lista vis){
-        //Metodo auxiliar, recorre los caminos desde nodo hasta encontrar a dest
-        //Almacena en vis los arcos ya recorridos
+    public boolean existeCaminoAux(NodoVert nodo, Object destino, Lista visitados){
+        //Metodo auxiliar, recorre los caminos desde nodo hasta encontrar a destino
+        //Almacena en visitados los arcos recorridos
         boolean exito = false;
         if (nodo != null){
-            if (dest.equals(nodo.getElemento())) { //Camino encontrado
+            if (destino.equals(nodo.getElemento())) { //Si nodo es destino, entonces hay camino
                 exito = true;
-            } else {
-                //Si no es el camino, verifico si hay camino entre nodo y dest
-                vis.insertar(nodo.getElemento(), vis.longitud()+1);
+            } else { //Si no es destino, verifico si hay camino entre nodo y destino
+                visitados.insertar(nodo.getElemento(), visitados.longitud()+1); //Agrego como visitado
                 NodoAdy ady = nodo.getPrimerAdyacente();
-                //Busco en todos sus adyacentes hasta encontrar dest
-                while (!exito && ady != null) {
-                    if (vis.localizar(ady.getVertice().getElemento()) < 0) {
-                        exito = existeCaminoAux(ady.getVertice(), dest, vis);
+                while (!exito && ady != null) { //Busco en todos sus adyacentes hasta encontrar destino
+                    if (visitados.localizar(ady.getVertice().getElemento()) < 0) { //Si no fue visitado
+                        exito = existeCaminoAux(ady.getVertice(), destino, visitados); //Lo visita
                     }
-                    ady = ady.getSigAdyacente();
+                    ady = ady.getSigAdyacente(); //Siguiente adyacente
                 }
             }
         }
         return exito;
     }
-    
+
     public Lista caminoMasCorto(Object origen, Object destino) {
-        Lista camMin = new Lista();
+        Lista camCorto = new Lista(); //Camino resultante
         NodoVert aux = this.inicio; 
         NodoVert auxOrig = null;
         NodoVert auxDest = null;
+        //No utilizo ubicarVertice dos veces pues es ineficiente
         while (aux != null && (auxOrig == null || auxDest == null)) { //Busco ambos nodos
-            if (origen.equals(aux.getElemento())) {
+            if (origen.equals(aux.getElemento())) { //Si encuentra origen
                 auxOrig = aux;
             } else {
-                if (destino.equals(aux.getElemento())) {
+                if (destino.equals(aux.getElemento())) { //Si encuentra destino
                     auxDest = aux;
                 }
             }
-            aux = aux.getSigVertice();
+            aux = aux.getSigVertice(); //Siguiente vertice
         }
         if (auxOrig != null && auxDest != null) { //Fueron encontrados, busco un camino
             Lista camActual = new Lista();
-            camMin = caminoCortoAux(auxOrig, destino, camMin, camActual);
+            camCorto = caminoCortoAux(auxOrig, destino, camCorto, camActual);
         }
-        return camMin;
+        return camCorto;
     }
 
-    /*Modificaciones realizadas*/
-    private Lista caminoCortoAux(NodoVert nodo, Object destino, Lista camMin, Lista camActual) {
+    private Lista caminoCortoAux(NodoVert nodo, Object destino, Lista camCorto, Lista camActual) {
+        //Metodo auxiliar, recursivamente recorre nodos de un grafo, retorna el camino mas corto desde nodo a destino
         if (nodo != null) {
-            Object elem = nodo.getElemento();
-            //Inserta nodo actual
-            camActual.insertar(elem, camActual.longitud()+1);
-            //Si encuentra destino
-            if (elem.equals(destino)) {
-                //Verifica que camActual es menor a camMin
-                if (camMin.esVacia() || (camMin.longitud() > camActual.longitud())) {
-                    //Clona camActual para no modificar la referencia
-                    camMin = camActual.clone();
-                }
-            } else {
-                NodoAdy ady = nodo.getPrimerAdyacente();
-                NodoVert auxVert = null;
-                //Recorro sus adyacentes 
-                while (ady != null) {
-                    auxVert = ady.getVertice();
-                    //Si no fue visitado en este camino, entonces lo visita
-                    if (camActual.localizar(auxVert.getElemento()) < 0) { 
-                        camMin = caminoCortoAux(auxVert, destino, camMin, camActual); 
+            //Verifica si no supera al mas corto con la siguiente insercion (ya que no tendria sentido seguir buscando en ese punto)
+            if (camCorto.esVacia() || ((camActual.longitud()+1) < camCorto.longitud())) {
+                Object elem = nodo.getElemento();
+                camActual.insertar(elem, camActual.longitud()+1); //Inserta nodo actual (pues es recorrido)
+                    if (elem.equals(destino)) { //Si encuentra destino
+                        //Clona camActual para no modificar la referencia
+                        camCorto = camActual.clone();
+                    } else {
+                        //Verifica si no supera al mas corto con la siguiente insercion (ya que no tendria sentido seguir buscando en ese punto)
+                        if (camCorto.esVacia() || ((camActual.longitud()+1) < camCorto.longitud())) {
+                            NodoAdy ady = nodo.getPrimerAdyacente();
+                            NodoVert auxVert = null;
+                            while (ady != null) { //Recorro sus adyacentes (arcos)
+                                auxVert = ady.getVertice(); 
+                                //Si no fue visitado en este camino, entonces lo visita
+                                if (camActual.localizar(auxVert.getElemento()) < 0) { 
+                                    camCorto = caminoCortoAux(auxVert, destino, camCorto, camActual); 
+                                }
+                                ady = ady.getSigAdyacente(); //Siguiente arco
+                            }
+                        }
                     }
-                    //Verifica siguiente arco
-                    ady = ady.getSigAdyacente();
                 }
-            }
-            //Quita nodo actual ya que puede haber mas caminos que lo visiten
+            //Quita nodo actual (ultimo insertado) porque puede haber mas caminos que lo visiten
             camActual.eliminar(camActual.longitud());
         }
-        return camMin;
+        return camCorto;
     }
-
+    
     public Lista caminoMasLargo(Object origen, Object destino) {
-        Lista camMax = new Lista();
+        Lista camLargo = new Lista();
         NodoVert aux = this.inicio; 
         NodoVert auxOrig = null;
         NodoVert auxDest = null;
+        //No utilizo ubicarVertice dos veces pues es ineficiente
         while (aux != null && (auxOrig == null || auxDest == null)) { //Busco ambos nodos
-            if (origen.equals(aux.getElemento())) {
+            if (origen.equals(aux.getElemento())) { //Si encuentra origen
                 auxOrig = aux;
             } else {
-                if (destino.equals(aux.getElemento())) {
+                if (destino.equals(aux.getElemento())) { //Si encuentra destino
                     auxDest = aux;
                 }
             }
-            aux = aux.getSigVertice();
+            aux = aux.getSigVertice(); //Siguiente vertice
         }
-        if (auxOrig != null && auxDest != null) { //si ambos fueron encontrados, busco un camino
+        if (auxOrig != null && auxDest != null) { //Fueron encontrados, busco un camino
             Lista camActual = new Lista();
-            camMax = caminoLargoAux(auxOrig, destino, camMax, camActual);
+            camLargo = caminoLargoAux(auxOrig, destino, camLargo, camActual);
         }
-        return camMax;
+        return camLargo;
     }
 
-    private Lista caminoLargoAux(NodoVert nodo, Object destino, Lista camMax, Lista camActual) {
+    private Lista caminoLargoAux(NodoVert nodo, Object destino, Lista camLargo, Lista camActual) {
+        //Metodo auxiliar, recursivamente recorre nodos de un grafo, retorna el camino mas largo
          if (nodo != null) {
+            //No verifica como en caminoCortoAux pues no es necesario (justamente se busca el mas largo)
             Object elem = nodo.getElemento();
-            //Inserta nodo actual
-            camActual.insertar(elem, camActual.longitud()+1);
-            //Si encuentra destino
-            if (elem.equals(destino)) {
-                //Verifica que camActual es mayor a camMax
-                if (camMax.longitud() < camActual.longitud()) {
+            camActual.insertar(elem, camActual.longitud()+1); //Inserta nodo actual
+            if (elem.equals(destino)) { //Si encuentra destino
+                //Verifica que camActual es mayor a camLargo
+                if (camActual.longitud() > camLargo.longitud()) {
                     //Clona camActual para no modificar la referencia
-                    camMax = camActual.clone(); 
+                    camLargo = camActual.clone(); 
                 }
             } else {
+                //No verifica como en caminoCortoAux pues no es necesario (justamente se busca el mas largo)
                 NodoAdy ady = nodo.getPrimerAdyacente();
                 NodoVert auxVert = null;
-                //Recorro sus adyacentes
-                while (ady != null) {
+                while (ady != null) { //Recorro sus adyacentes (arcos)
                     auxVert = ady.getVertice();
                     //Si no fue visitado en este camino, entonces lo visita
                     if (camActual.localizar(auxVert.getElemento()) < 0) {
-                        camMax = caminoLargoAux(auxVert, destino, camMax, camActual); 
+                        camLargo = caminoLargoAux(auxVert, destino, camLargo, camActual); 
                     }
-                    //Verifica siguiente arco
-                    ady = ady.getSigAdyacente();
+                    ady = ady.getSigAdyacente(); //Siguiente arco
                 }
             }
-            //Quita nodo actual ya que puede haber mas caminos que lo visiten
+            //Quita nodo actual (ultimo nodo insertado)) ya que puede haber mas caminos que lo visiten
             camActual.eliminar(camActual.longitud());
         }
-        return camMax;
+        return camLargo;
     }
 
     public Lista listarEnProfundidad() {
-        //Visita cada vertice y listaEnProfundidad cada adyacente
+        //Visita cada vertice y lista en profundidad cada adyacente
         Lista visitados = new Lista();
+        //Define vertice desde donde comenzar
         NodoVert aux = this.inicio;
         while (aux != null) { 
-            //Si el vertice no fue visitado
-            if (visitados.localizar(aux.getElemento()) < 0) {
+            if (visitados.localizar(aux.getElemento()) < 0) { //Si el vertice no fue visitado
                 listarEnProfundidadAux(aux, visitados);
             }
             aux = aux.getSigVertice();
@@ -360,15 +356,12 @@ public class GrafoEtiquetado {
     private void listarEnProfundidadAux(NodoVert nodo, Lista visitados) {
         //Lista cada nodo adyacente al nodo vertice ingresado si no fueron listados
         if (nodo != null) {
-            //Visita nodo
+            //Marca nodo como visitado
             visitados.insertar(nodo.getElemento(), visitados.longitud()+1);
             NodoAdy ady = nodo.getPrimerAdyacente();
-            NodoVert auxVert = null;
-            //Visita a los adyacentes que no fueron visitados
-            while (ady != null) {
-                auxVert = ady.getVertice();
-                if (visitados.localizar(auxVert.getElemento()) < 0) {
-                    listarEnProfundidadAux(auxVert, visitados);
+            while (ady != null) { //Visita a los adyacentes que no fueron visitados
+                if (visitados.localizar(ady.getVertice().getElemento()) < 0) {
+                    listarEnProfundidadAux(ady.getVertice(), visitados);
                 }
                 ady = ady.getSigAdyacente();
             }
@@ -376,11 +369,12 @@ public class GrafoEtiquetado {
     }
 
     public Lista listarEnAnchura() {
+        //Visita cada vertice y lista en anchura cada adyacente
         Lista visitados = new Lista();
+        //Define vertice desde donde comenzar
         NodoVert aux = this.inicio;
         while (aux != null) { 
-            //Si el vertice no fue visitado
-            if (visitados.localizar(aux.getElemento()) < 0) {
+            if (visitados.localizar(aux.getElemento()) < 0) { //Si el vertice no fue visitado
                 anchuraDesde(aux, visitados);
             }
             aux = aux.getSigVertice();
@@ -396,13 +390,12 @@ public class GrafoEtiquetado {
             NodoVert aux = (NodoVert) q.obtenerFrente();
             q.sacar();
             NodoAdy ady = aux.getPrimerAdyacente();
-            while (ady != null) {
+            while (ady != null) { //Para cada adyacente de aux (sacado de cola)
                 NodoVert auxVert = ady.getVertice();
-                //Si fue visitado
-                if (visitados.localizar(auxVert.getElemento()) < 0) {
+                if (visitados.localizar(auxVert.getElemento()) < 0) { //Si no fue visitado
                     //Visito y recorro por anchura
                     visitados.insertar(auxVert.getElemento(), visitados.longitud()+1);
-                    q.poner(auxVert);
+                    q.poner(auxVert); //Sera agregado a la cola del primer while
                 }
                 ady = ady.getSigAdyacente();
             }
@@ -423,10 +416,8 @@ public class GrafoEtiquetado {
         //Retorna una cadena con el contenido del grafo, nodo no debe ser vacio
         String ret = "";
         NodoAdy ady = nodo.getPrimerAdyacente();
-        //Concatena vertice
-        ret = "+ ("+nodo.getElemento()+"): \n       ";
-        //Concatena todos sus adyacentes
-        if (ady != null) {
+        ret = "+ ("+nodo.getElemento()+"): \n       "; //Concatena vertice
+        if (ady != null) { //Concatena todos sus adyacentes
             while (ady != null){
                 ret = ret + "->  "+ady.getVertice().getElemento()+": "+ady.getEtiqueta()+"\n       ";
                 ady = ady.getSigAdyacente();
@@ -434,14 +425,13 @@ public class GrafoEtiquetado {
         } else {
             ret = ret + "------ \n";
         }
-        //Concatena siguiente vertice
-        nodo = nodo.getSigVertice();
+        nodo = nodo.getSigVertice(); //Concatena siguiente vertice
         if (nodo != null){
             ret = ret + "\n "+toStringAux(nodo);
         }
         return ret;
     }
 
-    /*Clone */
+    /*Clone: clonar cada nodo vertice y sus adyacentes*/
 
 }
